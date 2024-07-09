c30abee48dca1f07596c64e82fa641177cedc8d2
sdmserg <sdmserg2021@gmail.com>
2024-07-09 22:26:35 +0300

    6. Правила Checkstyle [#504981]. Изменение класса Broken.

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -11,15 +11,15 @@ public class Broken {
 
     public Broken() { }
 
-    void echo() { }
+    public void echo() { }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
 
     }
 }
