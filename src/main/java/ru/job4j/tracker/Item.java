4710eb91cb7ad77f136872ce571542b6cd0e19ac
sdmserg <sdmserg2021@gmail.com>
2024-07-22 01:12:30 +0300

    8. Date. Отображение даты. [#310017]. Изменние класса Item, добаление класса StartUI

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,19 +1,27 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
+
     private String name;
 
+    private LocalDateTime created;
+
     public Item() {
+        this.created = LocalDateTime.now();
     }
 
     public Item(String name) {
         this.name = name;
+        this.created = LocalDateTime.now();
     }
 
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+        this.created = LocalDateTime.now();
     }
 
     public int getId() {
@@ -30,5 +38,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
