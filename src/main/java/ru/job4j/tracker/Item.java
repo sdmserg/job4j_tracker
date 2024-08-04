c3b02769809a23f8adc875a59f1213155a3fde26
sdmserg <sdmserg2021@gmail.com>
2024-08-04 19:49:21 +0300

    8. Date. Отображение даты. [#310017]. Изменние класса Item.

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,21 +7,18 @@ public class Item {
 
     private String name;
 
-    private LocalDateTime created;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
-        this.created = LocalDateTime.now();
     }
 
     public Item(String name) {
         this.name = name;
-        this.created = LocalDateTime.now();
     }
 
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
-        this.created = LocalDateTime.now();
     }
 
     public int getId() {
