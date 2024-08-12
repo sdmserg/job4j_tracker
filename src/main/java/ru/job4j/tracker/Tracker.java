42747a9a733c3f75564726e0cfe71dc0a63c5477
sdmserg <sdmserg2021@gmail.com>
2024-08-13 00:24:31 +0300

    5. Tracker - хранилище [#396]. Изменение классов Tracker, TrackerTest

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,10 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
     }
 
     public Item findById(int id) {
@@ -21,5 +27,17 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (key.equals(item.getName())) {
+                result[count++] = item;
+            }
+        }
+        return Arrays.copyOf(result, count);
     }
 }
