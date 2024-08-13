800a31d58837ff3463ef766c5c2a423fbc79711e
sdmserg <sdmserg2021@gmail.com>
2024-08-14 00:57:39 +0300

    10. toString [#310018] . Изменение классов Item, StartUI

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 
 public class Item {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+
     private int id;
 
     private String name;
@@ -39,5 +42,14 @@ public class Item {
 
     public LocalDateTime getCreated() {
         return created;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                 + ", created=" + created.format(FORMATTER)
+                + '}';
     }
 }
