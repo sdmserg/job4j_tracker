b766c55f331f0af7ed7fb8342405e78a7ebd6e4e
sdmserg <sdmserg2021@gmail.com>
2024-08-22 00:17:14 +0300

    3.3. Реализация класса StartUI. Изменение заявки [#500746 #509715]. Изменение класса StartUI.

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -29,9 +29,9 @@ public class StartUI {
                 }
             } else if (select == 2) {
                 System.out.println("=== Редактирование заявки ===");
-                System.out.println("Введите id: ");
+                System.out.print("Введите id: ");
                 int id = Integer.parseInt(scanner.nextLine());
-                System.out.println("Введите имя: ");
+                System.out.print("Введите имя: ");
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 if (tracker.replace(id, item)) {
