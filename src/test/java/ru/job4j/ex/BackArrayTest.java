a1ff22772ccda0d82019c0e8a80a53eabd002681
sdmserg <sdmserg2021@gmail.com>
2024-09-10 22:07:06 +0300

    0. Что такое исключение. [#3821]. Добавление классов BackArray и BackArrayTest.

---
+++ src/test/java/ru/job4j/ex/BackArrayTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.ex;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BackArrayTest {
+    @Test
+    void whenInputArrayhasEvenAmountElements() {
+        String[] names = {"Petr", "Ivan", "Nik", "Vasya"};
+        String[] result = new BackArray().turn(names);
+        String[] expected = {"Vasya", "Nik", "Ivan", "Petr"};
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenInputArrayHasNotEvenAmountElements() {
+        String[] names = {"Petr", "Ivan", "Andrey", "Nik", "Vasya"};
+        String[] result = new BackArray().turn(names);
+        String[] expected = {"Vasya", "Nik", "Andrey", "Ivan", "Petr"};
+        assertThat(result).isEqualTo(expected);
+    }
+}
