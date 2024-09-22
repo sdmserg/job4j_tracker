7ec6f09a04f0bd346dcd60e81dc1044a2b9edf87
sdmserg <sdmserg2021@gmail.com>
2024-09-22 23:17:41 +0300

    2.1. Тест на ValidateInput [#311887] . Изменние класса StartUI, добавление класса ValidateInputTest.

---
+++ src/test/java/ru/job4j/tracker/input/ValidateInputTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.tracker.input;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.output.StubOutput;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ValidateInputTest {
+
+    @Test
+    void whenInvalidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenValidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+             new String[] {"7"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(7);
+    }
+
+    @Test
+    void whenMultipleValidInput() {
+        Output output = new StubOutput();
+        String[] values = new String[] {"5", "7", "9", "11", "27"};
+        int[] numbers = {5, 7, 9, 11, 27};
+        Input in = new MockInput(values);
+        ValidateInput input = new ValidateInput(output, in);
+        for (int index = 0; index < values.length; index++) {
+            int selected = input.askInt("Enter menu:");
+            assertThat(selected).isEqualTo(numbers[index]);
+        }
+    }
+
+    @Test
+    void whenNegativeNumberInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"-9"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(-9);
+    }
+}
