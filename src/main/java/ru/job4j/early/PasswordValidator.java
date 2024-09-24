f8df77f28bef0b4b24b6f31438e7b05320102f86
sdmserg <sdmserg2021@gmail.com>
2024-09-25 00:29:42 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #514273]. Изменение класса PasswordValidator.

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -31,6 +31,9 @@ public class PasswordValidator {
             if (!Character.isLetterOrDigit(symbol)) {
                 hasSpecial = true;
             }
+            if (hasUpCase && hasLowCase && hasDigit && hasSpecial) {
+                break;
+            }
         }
         if (!hasUpCase) {
             throw new IllegalArgumentException(
