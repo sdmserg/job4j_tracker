182cf721f0a612d527f30fd23c4e86b26db9281f
sdmserg <sdmserg2021@gmail.com>
2024-09-25 00:33:03 +0300

    1. Singleton [#94606]. Добавление класса SingleTracker.

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,41 @@
+package ru.job4j.tracker;
+
+public final class SingleTracker {
+    public static SingleTracker instance = null;
+
+    private Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            return new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public void delete(int id) {
+        tracker.delete(id);
+    }
+}
