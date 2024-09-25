8088b354ad77f25b16d58f8ff13b996269f3cdde
sdmserg <sdmserg2021@gmail.com>
2024-09-26 02:48:18 +0300

    3. final [#334073]. Добавление классов Airport, Airbus, Aircraft.

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,34 @@
+package ru.job4j.ooa;
+
+public final class Airbus extends Aircraft {
+    private static final int COUNT_ENGINE = 2;
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        int countEngine = "A380".equals(name) ? 2 : COUNT_ENGINE;
+        System.out.println("Количество двигателей равно: " + countEngine);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
