bc33fa3d082691dff3347f58ee053e0cc02941b8
sdmserg <sdmserg2021@gmail.com>
2024-10-15 00:41:07 +0300

    0. Map, HashMap [#212652]. Добавление класса UsageMap.

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("sdmserg2021@gmail.com", "Petrov Sergei Viktorovich");
+        map.put("ivanovk@yandex.ru", "Ivanov Konstantin Sergeevich");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+
+    }
+}
