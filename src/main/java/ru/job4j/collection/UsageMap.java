debcd0fc4ed673c9b72811224983cf3a790dcfe8
sdmserg <sdmserg2021@gmail.com>
2024-10-17 02:40:32 +0300

    0. Map, HashMap [#212652 #517473] . Изменение класса UsageMap.

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,10 +7,15 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("sdmserg2021@gmail.com", "Petrov Sergei Viktorovich");
         map.put("ivanovk@yandex.ru", "Ivanov Konstantin Sergeevich");
+        map.put("petrodm@yeandex.ru", "Petrov Dmitry Alexandrovich");
+        map.put("smirnova@gmail.com", "Smirnova tatiana Sergeevna");
+        map.put("lebedev@yandex.ru", "Lebedev Georgy Alexeevich");
+        map.put("avgorbuneva@yeandex.ru", "Gorbuneva Anna Vasilievna");
+        map.put("petrodm@yeandex.ru", "Golubev Alexei Sergeevich");
+        map.put("smirnova@gmail.com", "Michalkova Daria Ivanovna");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
         }
-
     }
 }
