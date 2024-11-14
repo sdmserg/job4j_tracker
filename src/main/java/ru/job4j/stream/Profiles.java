f0524a77005c4bb4a2009f00a744eaac5c924a30
sdmserg <sdmserg2021@gmail.com>
2024-11-15 02:33:27 +0300

    7. Уникальность элементов и сортировка. [#110225]. Изменние классов Address, Profiles, ProfilesTest.

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -2,11 +2,21 @@ package ru.job4j.stream;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.Comparator;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        List<Address> addresses = collect(profiles);
+        Comparator<Address> comparator = Comparator.comparing(Address::getCity);
+        return addresses.stream()
+                .sorted(comparator)
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
