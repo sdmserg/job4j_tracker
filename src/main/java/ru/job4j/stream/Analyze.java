485c982b135011793e29906f83da1ad184ff529a
sdmserg <sdmserg2021@gmail.com>
2024-11-22 00:16:51 +0300

    3. Аттестация [#49777]. Изменение метода bestSubject в классе Analyze.

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -63,7 +63,6 @@ public class Analyze {
                 flatMap(pupil -> pupil.subjects().stream())
                 .collect(Collectors.groupingBy(
                         Subject::name,
-                        LinkedHashMap::new,
                         Collectors.summingDouble(Subject::score)
                 ))
                 .entrySet()
