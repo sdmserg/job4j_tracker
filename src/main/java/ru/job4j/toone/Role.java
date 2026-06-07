f2d0bd8a3f3bb89272bfa89cae5d2763881d7bd3
sergei <sdmserg2021@gmail.com>
2026-06-07 20:12:44 +0300

    Add Role, User, UserMessenger entities with ManyToOne and OneToMany mappings

---
+++ src/main/java/ru/job4j/toone/Role.java
@@ -0,0 +1,20 @@
+package ru.job4j.toone;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "j_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    private String name;
+}
